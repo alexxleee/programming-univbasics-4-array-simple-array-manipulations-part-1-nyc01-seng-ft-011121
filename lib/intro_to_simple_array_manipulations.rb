4dc07be4d090cf16a852e8c1d5847831b88b7cf6
Jung Ho Lee <alexxleee@gmail.com>
2020-12-31 21:32:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_pop(array)
+  continent = ["Africa", "Asia"]
+  continent.pop
+  array.pop
+end
+
+def using_pop2(array)
+  array.pop
+end
+
+
+p using_pop(["a", "b", "c"])
+
+p using_pop2(["a", "b", "c"])
+
